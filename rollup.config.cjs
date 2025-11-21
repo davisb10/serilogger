810bd27dc28f9b68e71ff99916b78c81094575d9
Brycen Davis <brycen.davis@sheakley.com>
2025-11-21 13:44:07 -0500

    Tweak rollup and tsconfig

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,34 +1,31 @@
-import typescript from 'rollup-plugin-typescript2';
 import stripCode from 'rollup-plugin-strip-code';
+import typescript from 'rollup-plugin-typescript2';
 
-export default [{
-    input: 'src/index.ts',
-    output: {
-        file: 'dist/serilogger.es6.js',
-        format: 'es',
-        name: 'serilogger',
-        sourcemap: true
-    },
-    plugins: [typescript({
-        target: 'es6',
-        typescript: require('typescript')
-    })]
-}, {
-    input: 'src/index.ts',
-    output: {
-        file: 'dist/serilogger.js',
-        format: 'umd',
-        name: 'serilogger',
-        sourcemap: true
-    },
-    plugins: [
-        typescript({
-            target: 'es5',
-            typescript: require('typescript')
-        }),
-        stripCode({
-            start_comment: 'start_test_code',
-            end_comment: 'end_test_code'
-        })
-    ]
-}];
+export default [
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'dist/serilogger.es6.js',
+			format: 'es',
+			name: 'serilogger',
+			sourcemap: true
+		},
+		plugins: [typescript()]
+	}, 
+	{
+		input: 'src/index.ts',
+		output: {
+			file: 'dist/serilogger.js',
+			format: 'umd',
+			name: 'serilogger',
+			sourcemap: true
+		},
+		plugins: [
+			typescript(),
+			stripCode({
+				start_comment: 'start_test_code',
+				end_comment: 'end_test_code'
+			})
+		]
+	}
+];
